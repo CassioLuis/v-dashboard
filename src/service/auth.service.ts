df2098bcbb861f53a1754014d35dea5442ebd429
CassioLuis <cassiocaruzo@gmail.com>
2023-10-09 09:19:46 -0300

    atualiza url

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import type LoginContract from './interfaces/auth'
 
-// const url = 'https://player-api-z98a.onrender.com/'
-const url = 'http://localhost:3000/'
+const url = 'https://player-api-z98a.onrender.com/'
+// const url = 'http://localhost:3000/'
 
 export default class User {
   static async login(login: LoginContract): Promise<any> {
