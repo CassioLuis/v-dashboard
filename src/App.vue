e37e329cff89de5372d6d3107f0c7fa5a6ffbeeb
root <root@Cassio>
2024-10-21 10:21:19 -0300

    melhoria meta pixel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const { currentRoute } = useRouter()
 
 const layout = computed(() => currentRoute.value.meta.layout || 'DefaultLayout')
+
+declare global {
+  interface Window {
+    fbq: any
+  }
+}
+
+onMounted (() => {
+  // Carrega o script do Facebook Pixel
+  (function (f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+    if (f.fbq)
+      return
+    n = f.fbq = function (...args: any[]) {
+      n.callMethod
+        ? n.callMethod(...args)
+        : n.queue.push(args)
+    }
+    if (!f._fbq) {
+      f._fbq = n
+      n.push = n
+      n.loaded = true
+      n.version = '2.0'
+    }
+    n.queue = []
+    t = b.createElement(e)
+    t.async = true
+    t.src = v
+    t.onload = () => console.log('Facebook Pixel script loaded successfully')
+    t.onerror = () => console.error('Facebook Pixel script blocked or failed to load')
+    s = b.getElementsByTagName(e)[0]
+    s.parentNode.insertBefore(t, s)
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js')
+
+  // Inicializa o Facebook Pixel
+  if (window.fbq) {
+    window.fbq('init', '1362202448556022')
+    window.fbq('track', 'PageView')
+  }
+  else {
+    console.warn('Facebook Pixel could not be initialized.')
+  }
+})
 </script>
 
 <template>
